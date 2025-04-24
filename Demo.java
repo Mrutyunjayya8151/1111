d0aba83302fdfd85501ce11485f1c671b2f09808
Mrutyunjayya8151 <Mrityunjayyanp@gmail.com>
2025-04-24 15:54:40 +0530

    save file

---
+++ Demo.java
@@ -0,0 +1,12 @@
+public class Demo{
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+
+        System.out.println("Addition is: " + sum);
+        System.out.println("Subtraction is: " + difference);
+    }
+}
