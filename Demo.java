2e9fead61838e3a0316f9b4f325b2c873e523aa0
Mrutyunjayya8151 <Mrityunjayyanp@gmail.com>
2025-04-24 16:12:11 +0530

    save file

--- Demo.java
+++ Demo.java
@@ -2,11 +2,16 @@ public class Demo{
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 5;
+        
 
         int sum = num1 + num2;
         int difference = num1 - num2;
+        int product = num1 * num2;
+        int Division= num1/num2;
 
         System.out.println("Addition is: " + sum);
         System.out.println("Subtraction is: " + difference);
+        System.out.println("Multiplication is: " + product);
+        System.out.println("Division is: " + Division);
     }
 }
